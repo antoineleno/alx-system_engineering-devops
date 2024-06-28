dc74c7c575564b5632012b6b7c4a2339fb25a47b
antoineleno <johndoe@example.com>
2024-06-28 21:57:21 +0800

    cm

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # Install falsk version 2.1.0 using pip3
-package { 'flask':
-  ensure   => 2.1.0,
-  provider => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
