823dfea43eef585b7c64aec7ce42dde3d8fef74e
antoineleno <johndoe@example.com>
2024-06-28 21:54:47 +0800

    CM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Install falsk version 2.1.0 using pip3
-package { 'flask': # Antoine
-  ensure   => '2.1.0', #Antoine
-  provider => 'pip3', # Antoine
+package { 'flask':
+  ensure   => 2.1.0,
+  provider => 'pip3',
 }
