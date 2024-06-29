ccdb7de3d61d0d02224a963cb7a5763746ad161d
antoineleno <141551233+antoineleno@users.noreply.github.com>
2024-06-29 15:16:51 +0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,29 +1,15 @@
-#!/usr/bin/env
-# Define the SSH client configuration file for global settings
-file { '/etc/ssh/ssh_config':
-  ensure  => present,
-  mode    => '0644',
-  content => "
-    # Global SSH client configuration
-    Host *
-        PasswordAuthentication no
-  ",
+# make changes to config file using Puppet
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
-# Define the SSH client configuration file for user-specific settings (root user)
-file { '/root/.ssh/config':
-  ensure  => present,
-  mode    => '0600',
-  content => template('module_name/ssh_config.erb'),
-}
-
-# Define the template for user-specific SSH client configuration
-file { 'module_name/ssh_config.erb':
-  ensure  => present,
-  mode    => '0644',
-  content => "
-    # User-specific SSH client configuration
-    Host your_server_hostname
-        IdentityFile ~/.ssh/school
-  ",
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
