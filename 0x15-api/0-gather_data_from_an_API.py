3d24dec6a57af50c0226576ed6ada79ef43a60a7
antoineleno <johndoe@example.com>
2024-07-31 02:02:06 +0800

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
 
-
-    user = requests.get(url + "users/{}".format(user_id)).json()
+    uer = requests.get(url + "users/{}".format(user_id)).json()
     to_do_list = requests.get(url + "todos", user_id).json()
 
     done_task_count = 0
@@ -23,7 +22,7 @@ if __name__ == "__main__":
             total_tasks += 1
 
     count = "{}/{}".format(done_task_count, total_tasks)
-    print("Employee {} is done with tasks({}):".format(user.get("name"), count))
+    print("Employee {} is done with tasks({}):".format(uer.get("name"), count))
 
     for activity in to_do_list:
         user_id_match = int(user_id) == activity.get("userId")
