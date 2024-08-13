af9e138194d341811ca700f5c1cdc0bd6f8d6eac
antoineleno <johndoe@example.com>
2024-08-14 03:51:40 +0800

    api advance

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,9 +11,10 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response_data = requests.get(url, headers={"User-Agent": "Mozilla/5.0"},
                                  allow_redirects=False)
-    if response_data.status_code != 200:
-        return 0
-    else:
+    if response_data.status_code == 200:
         data = response_data.json()
         subscribers = data['data']['subscribers']
         return subscribers
+    else:
+        return 0
+
