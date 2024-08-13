933df1aa2ce71aa0617f5c5e0ce7ec65805d1f80
antoineleno <johndoe@example.com>
2024-08-14 03:52:22 +0800

    aa

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
-Script to query Reddit API
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers on a given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response_data = requests.get(url, headers={"User-Agent": "Mozilla/5.0"},
-                                 allow_redirects=False)
-    if response_data.status_code == 200:
-        data = response_data.json()
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
-
