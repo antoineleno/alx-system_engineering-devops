6f859e5dfa5e389dda446a31455ff7ae00d988da
antoineleno <johndoe@example.com>
2024-08-14 03:54:50 +0800

    api advance

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Script to query Reddit API
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Return the number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
+    response_data = requests.get(url, headers={"User-Agent": "Mozilla/5.0"},
+                                 allow_redirects=False)
+    if response_data.status_code != 200:
+        return 0
+    else:
+        data = response_data.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
-        return 0
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,2 +1,3 @@
+# Resources
 * [Reddit API Documentation](https://www.reddit.com/dev/api/)
 * [Query String](https://en.wikipedia.org/wiki/Query_string)
