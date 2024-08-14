d7f28c18d832359bdccf6bd54db5d1c886e908ce
antoineleno <johndoe@example.com>
2024-08-15 00:32:43 +0800

    web stack debuggin

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,29 +1,6 @@
-# 0-strace_is_your_friend.pp
+# ind out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
 
-# Ensure Apache is installed
-package { 'apache2':
-  ensure => installed,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Ensure the necessary directories have correct permissions
-file { '/var/www/html':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'www-data',
-  group   => 'www-data',
-}
-
-# Ensure the Apache configuration is correct (example for a specific file)
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/apache/000-default.conf',
-  notify  => Service['apache2'], # Restart Apache if the config changes
-}
-
-# Ensure Apache service is running
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/apache2/sites-available/000-default.conf'], # Restart if config changes
-}
-
