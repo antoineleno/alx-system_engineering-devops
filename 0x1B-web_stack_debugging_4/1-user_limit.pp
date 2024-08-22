20bbcd7788b23cceef9cf7e294cf0f2daae3d2c2
antoineleno <141551233+antoineleno@users.noreply.github.com>
2024-08-22 13:43:32 +0800

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
-# Change the OS configuration file
-# allow  holberton user.
+# Change the OS configuration file of the server
+# allow  holberton user to have access.
 
 exec {'OS security config':
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
